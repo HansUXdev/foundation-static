1833bf89f4286100fbd9d6d54f04330e71b53a5f
Hans <bmm79@nau.edu>
2017-08-21 01:26:45 -0700

    changed these files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -252,7 +252,7 @@ function commit(cb){
 // Check the status of the repo, add all changes, 
 // commit changes and push
 
-gulp.task('heroku', function commit(cb){
+gulp.task('heroku', function (cb){
     var command = `git add -A && git commit -m "updating for heroku" && git push heroku master`
       // Run the command
       exec(`gulp`, function (err, stdout, stderr) {
